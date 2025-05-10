097283765217d121f267862de13cd02abec5a2ef
dwbwltrwbwl <tyumenelizaveta@yandex.ru>
2025-05-11 02:00:01 +0300

    Добавьте файлы проекта.

---
+++ students/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace students
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ApplicationData.AppConnect.model01 = new ApplicationData.studentsEntities();
+            ApplicationData.AppFrame.frmMain2 = frameMain;
+            frameMain.Navigate(new Pages.Autorization());
+        }
+    }
+}
---
+++ students/Pages/Autorization.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace students.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Autorization.xaml
+    /// </summary>
+    public partial class Autorization : Page
+    {
+        public Autorization()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonVhod_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonRegistr_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

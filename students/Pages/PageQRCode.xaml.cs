55b6b197b011a3063d2fd78b39f27e29c9ddf006
dwbwltrwbwl <tyumenelizaveta@yandex.ru>
2025-06-05 03:03:54 +0300

    0303

--- students/Pages/PageQRCode.xaml.cs
+++ students/Pages/PageQRCode.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ZXing;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace students.Pages
 {
@@ -23,8 +28,34 @@ namespace students.Pages
         public PageQRCode()
         {
             InitializeComponent();
+            LoadQR();
         }
+        private void LoadQR()
+        {
+            var writer = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new ZXing.Common.EncodingOptions
+                {
+                    Width = 300,
+                    Height = 300
+                }
+            };
+            var result = writer.Write(@"https://petersburgedu.ru/");
+            var bitmap = new BitmapImage();
 
+            using (var memoryStream = new MemoryStream())
+            {
+                result.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+                memoryStream.Position = 0;
+                bitmap.BeginInit();
+                bitmap.StreamSource = memoryStream;
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.EndInit();
+                bitmap.Freeze();
+            }
+            imgQr.Source = bitmap;
+        }
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.GoBack();
